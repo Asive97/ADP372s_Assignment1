91655d133671bf6e1f22076b0397b8e072a05b26
Asive madladla <asivemadladla@gmail.com>
2021-03-31 10:39:17 +0200

    initial commitment

--- src/main/java/za/ac/cput/ADP372s_Assignment1/TestDrivenDevelopment.java
+++ src/main/java/za/ac/cput/ADP372s_Assignment1/TestDrivenDevelopment.java
@@ -1,4 +1,30 @@
 package za.ac.cput.ADP372s_Assignment1;
 
 public class TestDrivenDevelopment {
+    private String id,name;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "TestDrivenDevelopment{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
 }
+
